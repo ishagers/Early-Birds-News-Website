871f63d9a56b5bcb056843a27b67ef95083c7317
JuanPGutierrezz <jpg55@njit.edu>
2024-02-26 13:51:16 -0500

    DB testing

--- IT490Web/rabbitmqphp_example/mysqlClient.php
+++ IT490Web/rabbitmqphp_example/mysqlClient.php
@@ -1,4 +1,6 @@
 <?php
+echo "Script started.<br>";
+
 // MySQL connection parameters
 $servername = "10.147.17.233";
 $username = "IT490DB";
@@ -11,24 +13,22 @@ $conn = new mysqli($servername, $username, $password, $database);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to database successfully.<br>";
 }
-echo "<p>Connected successfully</p>";
 
-$sql = "SELECT * FROM users"; 
+$sql = "SELECT * FROM users";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    echo "<table border='1'><tr><th>hash</th><th>Username</th><th>Email</th></tr>"; 
-
-    // Output data of each row
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row["hash"] . "</td><td>" . $row["username"] . "</td><td>" . $row["email"] . "</td></tr>";
-    }
-
-    // End table
-    echo "</table>";
+if ($result === false) {
+    echo "Error executing query: " . $conn->error . "<br>";
 } else {
-    echo "<p>0 results</p>";
+    if ($result->num_rows > 0) {
+        // Process your results here
+        echo "Query executed successfully, data retrieved.<br>";
+    } else {
+        echo "Query executed successfully, but no data found.<br>";
+    }
 }
 
 // Close connection
