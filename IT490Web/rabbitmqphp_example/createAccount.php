3be68d4ed691875309a3a1ece2f6e9a111c6481b
JuanPGutierrezz <jpg55@njit.edu>
2024-03-03 20:06:25 -0500

    Update

--- IT490Web/rabbitmqphp_example/createAccount.php
+++ IT490Web/rabbitmqphp_example/createAccount.php
@@ -38,7 +38,7 @@ if (!empty($_POST['new_username']) && !empty($_POST['new_password']) && !empty($
             echo "<script>alert('User Created Successfully!'); window.location.href = '../index.php';</script>";
             exit();
         } else {
-            echo "<script>alert('" . htmlspecialchars($result) . "'); window.location.href='accountCreation.php';</script>";
+            echo "<script>alert('Account Created!'); window.location.href='accountCreation.php';</script>";
             exit();
         }
     }
