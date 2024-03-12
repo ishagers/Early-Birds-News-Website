2bc146e0ca817a184271d743031890ed9e523cfd
IT490Project <it490project@IT490Ubuntu.myguest.virtualbox.org>
2024-03-12 11:13:04 -0400

    Changed Queues/Hosts to our 490 ones instead of test, if missing one look for it and change it

--- IT490Web/rabbitmqphp_example/NewsAPIData.php
+++ IT490Web/rabbitmqphp_example/NewsAPIData.php
@@ -1,5 +1,28 @@
 <?php
-$results = shell_exec('GET "Here Goes http link for api, delet double quotes"');
-$arrayCode = json_decode($results);
-var_dump($arrayCode);
+
+// The API endpoint with your API key
+$url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=898d8c1625884af1a9774e9662cb980d";
+
+// Initialize a cURL session
+$curl = curl_init($url);
+
+// Set options for the cURL session
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json',
+]);
+
+// Execute the cURL session and get the response
+$response = curl_exec($curl);
+
+// Close the cURL session
+curl_close($curl);
+
+// Decode the JSON response
+$responseData = json_decode($response);
+
+// Dump the decoded JSON to see the structure
+var_dump($responseData);
+
 ?>
+
