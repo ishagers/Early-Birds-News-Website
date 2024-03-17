e16e2dc254d4fc6a0965284e1ac3e5608e0abda6
Angel Espinal <ale24@njit.edu>
2024-03-17 17:05:41 -0400

    Update testRabbitMQClient.php

--- IT490Web/rabbitmqphp_example/testRabbitMQClient.php
+++ IT490Web/rabbitmqphp_example/testRabbitMQClient.php
@@ -4,7 +4,7 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-$client = new rabbitMQClient("testRabbitMQ.ini","490Exchange");
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 if (isset($argv[1]))
 {
   $msg = $argv[1];
