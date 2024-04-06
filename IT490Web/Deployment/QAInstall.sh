6d9363236430e64e99b949eba64ff4919ed89044
JuanPGutierrezz <jpg55@njit.edu>
2024-04-06 14:44:34 -0400

    QA/Prod Install Scripts

--- IT490Web/Deployment/QAInstall.sh
+++ IT490Web/Deployment/QAInstall.sh
@@ -6,7 +6,7 @@ user='IT490DB'
 password='IT490DB'
 database='Deployment'
 DeployIP="10.147.17.54" # Deployment machine IP
-
+Pass = "YogiMaster123@"
 # Prompt user for input
 echo "Please enter the QA (layer 2) machine (FE, BE, DMZ): "
 read machine
@@ -45,15 +45,15 @@ latestBundle=$(get_latest_bundle)
 if [[ -n "$latestBundle" ]]; then
     echo "Latest bundle found: $latestBundle"
     echo "Copying $latestBundle to QA machine..."
-    scp -o StrictHostKeyChecking=no "$path/$latestBundle/$bundleType.zip" "juanguti@$DeployIP:$installpath"
+    sshpass -v -p "$Pass" scp -o StrictHostKeyChecking=no "$path/$latestBundle/$bundleType.zip" "juanguti@$DeployIP:$installpath"
     
     echo "Unzipping $latestBundle on QA machine..."
-    ssh "juanguti@$DeployIP" "unzip -o $installpath/$bundleType.zip -d $installpath && rm $installpath/$bundleType.zip"
+    sshpass -p "$Pass" ssh "juanguti@$DeployIP" "unzip -o $installpath/$bundleType.zip -d $installpath && rm $installpath/$bundleType.zip"
     
     # Modify this with actual logic or service name
     yourServiceName="apache2" # Modify based on actual service needed
     echo "Restarting services on QA machine..."
-    ssh "juanguti@$DeployIP" "sudo systemctl restart $yourServiceName"
+    sshpass -p "$Pass" ssh "juanguti@$DeployIP" "sudo systemctl restart $yourServiceName"
     
     echo "QA installation and service restart completed."
 else
