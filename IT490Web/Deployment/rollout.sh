aa10e57b8b18ee34f6a5a8d5a0c4ac396d621f10
JuanPGutierrezz <jpg55@njit.edu>
2024-04-06 12:53:34 -0400

    deployment QA install update

--- IT490Web/Deployment/rollout.sh
+++ IT490Web/Deployment/rollout.sh
@@ -59,70 +59,11 @@ create_version_directory() {
     echo "$new_dir_name"
 }
 
-#!/bin/bash
-
-# Initialize version variable
-version=1
-
-# MySQL login and DB configurations
-user='IT490DB'
-password='IT490DB'
-database='Deployment'
-
-# Prompt user for input
-echo "Please enter the dev(layer 1) machine (FE, BE, DMZ): "
-read machine
-echo "Please Enter the config File Name(FE,BE,DMZ,DB,CSS .config): "
-read configFile
-
-# Set configuration based on machine type
-case $machine in
-    "FE")
-        path="/var/www/html/IT490-Project/IT490Web/Deployment"
-        devMachineName="juanguti"
-        devIP="10.147.17.233"
-        devPass="YogiMaster123@"
-        qaIP="10.147.17.67"
-        echo "Read FE machine details"
-        ;;
-    "BE")
-        path="/var/www/html/IT490-Project/IT490Web/Deployment"
-        devMachineName="ANGELTI490DEVUSERMACHINE"
-        devIP="10.147.17.90"
-        devPass="ANGELIT490DEVPASSWORD"
-        qaIP="****DEVLAYERIPHERE****"
-        echo "Read BE machine details"
-        ;;
-    "DMZ")
-        path="/var/www/html/IT490-Project/IT490Web/DMZ"
-        devMachineName="AngelDMZ490"
-        devIP="10.147.17.227"
-        devPass="dmz490"
-        qaIP="****DEVLAYERIPHERE****"
-        echo "Read DMZ machine details"
-        ;;
-    *)
-        echo "Error: Invalid machine type specified. Please enter 'FE', 'BE', or 'DMZ'."
-        exit 1
-        ;;
-esac
-
-# Function to determine the latest version for a specific machine type
-get_latest_version() {
-    echo $(mysql --user="$user" --password="$password" --database="$database" -sse "SELECT MAX(version) FROM versionHistory WHERE pkgName LIKE '$machine%'")
-}
-
-# Function to create a new version directory
-create_version_directory() {
-    new_version=$(($1 + 1))
-    new_dir_name="${machine}v${new_version}"
-    mkdir "$new_dir_name" && cd "$new_dir_name"
-    echo "$new_dir_name"
-}
-
 # Determine the latest version and create a new version directory
 latest_version=$(get_latest_version)
 version_dir=$(create_version_directory $latest_version)
+
+cd "$version_dir" || exit 1
 
    # Securely copy config file into folder
 sshpass -v -p "$devPass" scp -o StrictHostKeyChecking=no "$devMachineName@$devIP:$path/$configFile" "./$configFile"
@@ -156,4 +97,3 @@ echo "Cleanup complete."
     version_number="${version_dir: -1}" # Get the last character, which is the version number
     mysql --user="$user" --password="$password" --database="$database" -e "INSERT INTO versionHistory (version, pkgName, passed) VALUES ('$version_number', '$version_dir', NULL);"
     echo "Version: $version_number pushed with package name: $pkgName"
-
