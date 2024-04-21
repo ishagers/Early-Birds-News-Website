d7c22c1194d6b5491363793463db6a7414336a12
Angel Espinal <ale24@njit.edu>
2024-04-21 13:12:54 -0400

    Update session.php

--- IT490Web/rabbitmqphp_example/session.php
+++ IT490Web/rabbitmqphp_example/session.php
@@ -7,7 +7,8 @@ if (session_status() === PHP_SESSION_NONE) {
 
 // Function to check if the user is logged in
 function checkLogin() {
-    if (!isset($_SESSION['username'])) {
+    // Check for either username or user_id
+    if (!isset($_SESSION['username']) || !isset($_SESSION['user_id'])) {
         echo "<script>alert('Please log in first!');</script>";
         // Use header to redirect and then exit to stop further script execution
         header("Refresh: 0.1; url=../index.php");
