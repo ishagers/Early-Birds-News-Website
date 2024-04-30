603650f1c13b67d7db823ef1c0465f922b34daec
JuanPGutierrezz <jpg55@njit.edu>
2024-04-30 03:04:06 -0400

    2FA fix

--- IT490Web/index.php
+++ IT490Web/index.php
@@ -25,8 +25,14 @@ if (!empty($_POST['username']) && !empty($_POST['password'])) {
             'username' => $_POST['username'],
         ];
         $verificationResult = publisher($queryValues);
-        header("Location: verify.php"); // Server-side redirection
-
+        
+        if ($verificationResult) {
+            header("Location: verify.php"); // Server-side redirection
+            exit();
+        } else {
+            $errorMessage = isset($verificationResult['message']) ? $verificationResult['message'] : "Verification process failed.";
+            echo "<script>alert('" . htmlspecialchars($errorMessage) . "');</script>";
+        }
     } else {
         // Login failed or result is not properly formatted
         $errorMessage = isset($result['message']) ? $result['message'] : "Login failed. Please try again.";
