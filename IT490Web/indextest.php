32075bbb7ee9dc6950c6622e9f9cba697755a081
JuanPGutierrezz <jpg55@njit.edu>
2024-05-01 15:13:34 -0400

    2FE local change for testing

--- IT490Web/indextest.php
+++ IT490Web/indextest.php
@@ -1,7 +1,7 @@
 <?php
 ob_start();
 session_start();
-require('databaseFunctions.php'); // Database connection and utility functions
+require('rabbitmqphp_example/databaseFunctions.php'); // Database connection and utility functions
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
